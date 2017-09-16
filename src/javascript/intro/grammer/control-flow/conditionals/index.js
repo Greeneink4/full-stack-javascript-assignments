45ba2faff24c9190c8d77d25fa935adb78c7a364
michael Greene <greeneink4@gmail.com>
2017-09-16 11:56:56 -0600

    Conditionals Test

--- src/javascript/intro/grammer/control-flow/conditionals/index.js
+++ src/javascript/intro/grammer/control-flow/conditionals/index.js
@@ -1,8 +1,27 @@
-export const greeting = (age = 0) => {
+export const greeting = (age = 0) => age > 18 ? 'Hello!' : 'Yo!';
 
-};
+    // if (age > 18) {
+    //     return 'Hello';
+    // } else {
+    //     return 'Yo';
+    // }
+
 
 export const isTacoTuesday = (day = '') => {
+    switch(day) {
+        case 'monday':
+        case 'Wednesday':
+        case 'thursday':
+        case 'friday':
+        case 'saturday':
+        case 'sunday':
+            return false;
+            break;
+        case 'tuesday':
+            return true;
+        default:
+            console.log(day + 'is not a day of the week.');
+    }
 
 };
 
--- src/javascript/intro/grammer/strings/index.js
+++ src/javascript/intro/grammer/strings/index.js
@@ -1,1 +1,2 @@
-export const greeting = '';
+export const greeting = 'Hello World';
+export const characterAt = greeting[4];
